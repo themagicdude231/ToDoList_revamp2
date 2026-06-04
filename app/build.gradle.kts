5999954b7e433db335afa13c389ddef370113794
themagicdude231 <themagicdude231@gmail.com>
2026-06-04 13:59:28 +0800

    Changes by vosotros

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.14.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
