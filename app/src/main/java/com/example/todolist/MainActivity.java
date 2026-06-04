5a1de05c20148996697f5ae649274c33dcfcd4dc
themagicdude231 <themagicdude231@gmail.com>
2026-06-04 23:06:12 +0800

    Changes by vosotros

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         initViews();
     }
     private void initViews(){
-
+        // before back end:
+        // create design for ai chatbot and for the add task interface (modal layout inflater + alertdialog.builder)
+        // improvement in design especially in main menu
     }
 }
